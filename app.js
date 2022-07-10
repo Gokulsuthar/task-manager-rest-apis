19bd2687b025d81259e2cbc68290d81f63a41859
Gokul Suthar <Gokulsutharofficial@gmail.com>
2022-07-10 14:28:24 +0530

    first commit

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ app.enable('trust proxy');
 app.use(cors());
 // Access-Control-Allow-Origin *
 // api.natours.com, front-end natours.com
-app.use(cors({
-  origin: 'https://task-manager-rest-apis.herokuapp.com'
-}))
+// app.use(cors({
+//   origin: 'https://task-manager-rest-apis.herokuapp.com'
+// }))
 
 // app.options('*', cors());
 // app.options('/api/v1/tours/:id', cors());
 
 // Set security HTTP headers
-// app.use(helmet());
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
